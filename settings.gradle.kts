ebe13e3ab356e3b67b94a520907622a8d15e8640
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-04-20 23:09:21 +0800

    fixed package name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "mirai-console-plugin-template"
+rootProject.name = "2022OOP"
--- src/main/kotlin/PluginMain.kt
+++ src/main/kotlin/PluginMain.kt
@@ -1,4 +1,4 @@
-package org.example.mirai.plugin
+package org.ritsu.mirai.plugin
 
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
