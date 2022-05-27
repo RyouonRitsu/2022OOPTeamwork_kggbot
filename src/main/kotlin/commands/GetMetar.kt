8a374a03ab9ee1de35409b39fc02a01b9c023a4a
Faker <75046675@qq.com>
2022-05-27 23:04:53 +0800

    修改getMetar功能

--- src/main/kotlin/commands/GetMetar.kt
+++ src/main/kotlin/commands/GetMetar.kt
@@ -2,23 +2,34 @@ package org.ritsu.mirai.plugin.commands
 
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.net.Proxy
 
 fun getMetar(airport: String): String {
     val url = "https://aviationweather.gov/metar/data?ids=$airport&format=raw&date=&hours=0"
-    val client = OkHttpClient()
-    val request = Request.Builder().get().url(url).build()
-    val response = client.newCall(request).execute()
-    var body = response.body?.string() ?: return "无法获取body!\n"
-    return when (body.indexOf("No METAR found")) {
-        -1 -> {
-            try {
-                if (body.indexOf(">$airport") == -1) return "No METAR found"
-                body = body.substring(body.indexOf(">$airport") + 1, body.length)
-                body.substring(0, body.indexOf("</code>"))
-            } catch (e: Exception) {
-                "解析错误: ${e.message}\n"
-            }
+    val client = OkHttpClient().also {
+        it.newBuilder().apply {
+            proxy(Proxy.NO_PROXY)
         }
-        else -> "No METAR found"
+    }
+    val request = Request.Builder().get().url(url).build()
+    try {
+        val response = client.newCall(request).execute()
+        var body = response.body?.string() ?: return "无法获取body!\n"
+        return when (body.indexOf("No METAR found")) {
+            -1 -> {
+                try {
+                    if (body.indexOf(">$airport") == -1) return "No METAR found"
+                    body = body.substring(body.indexOf(">$airport") + 1, body.length)
+                    body.substring(0, body.indexOf("</code>"))
+                } catch (e: Exception) {
+                    "解析错误: ${e.message}\n"
+                }
+            }
+            else -> "No METAR found"
+        }
+    } catch (e: java.net.SocketTimeoutException) {
+        return "连接超时"
+    } catch (e: Exception) {
+        return "未知错误"
     }
 }
