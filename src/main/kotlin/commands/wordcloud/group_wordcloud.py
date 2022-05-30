d00c9932a32a932092f98d6fb65012346ea037b3
RyouonRitsu <zhou.hongxi0903@qq.com>
2022-05-30 17:38:39 +0800

    优化词云 更新停止词

--- src/main/kotlin/commands/wordcloud/group_wordcloud.py
+++ src/main/kotlin/commands/wordcloud/group_wordcloud.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     with open("./stopwords_list.txt", "r", encoding="utf-8") as f:
         stopwords = f.read().split("\n")
     new_text = " ".join(jieba.cut(text))
-    wc = WordCloud(font_path="STCAIYUN.TTF", background_color="white", width=800, height=400, stopwords=set(stopwords))\
+    wc = WordCloud(font_path="STCAIYUN.TTF", background_color="white", width=1920, height=1080,
+                   stopwords=set(stopwords), max_words=400) \
         .generate(new_text)
     wc.to_file(f"../../../../../data/{group}.png")
